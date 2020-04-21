92f30b19eaa9770ba09c21085812e6103194ac84
Khanh Vu <kvu9@lsu.edu>
2020-04-21 13:43:20 -0500

    finished lab 9

--- map3/map3.js
+++ map3/map3.js
@@ -3,5 +3,27 @@ var map3Object = L.map('map3Div').setView([32.18, -99.14], 4)
 L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png').addTo(map3Object)
 var stateUrl = 'https://geog4046.github.io/assignment-resources/data/us_state_demographics_ESRI_2010A.geojson'
 jQuery.getJSON(stateUrl, function (data) {
-  L.geoJSON(data, { style: { color: 'green' } }).addTo(map3Object)
+  var stateStyle = function (feature) {
+    var males = feature.properties.MALES
+    var females = feature.properties.FEMALES
+    var stateColor = 'blue'
+    if (males < females) { stateColor = 'red' }
+    return {
+      color: stateColor,
+      weight: 1,
+      fillOpacity: 0.3,
+      fill: true
+    }
+  }
+  var popup = function (feature, layer) {
+    var name = feature.properties.STATE_NAME
+    var males = feature.properties.MALES
+    var females = feature.properties.FEMALES
+    layer.bindPopup('State name: ' + name + '<br>Males: ' + males + '<br>Females: ' + females)
+  }
+  var options = {
+    style: stateStyle,
+    onEachFeature: popup
+  }
+  L.geoJSON(data, options).addTo(map3Object)
 })
